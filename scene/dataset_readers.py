0d244dad68d64a52c4fa3dad532ee427df6cf29a
youjeongd <yjdoh0918@naver.com>
2025-12-04 16:35:09 +0900

    fix: eval = True

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -304,6 +304,8 @@ def readColmapSceneInfo(path, images, eval, n_views=0, llffhold=8):
     cam_infos = sorted(cam_infos_unsorted.copy(), key = lambda x : x.image_name)
 
     print ("eval:", eval)
+    eval = True
+    print ("eval:", eval)
 
     if eval:
         train_cam_infos = [c for idx, c in enumerate(cam_infos) if idx % llffhold != 0]
